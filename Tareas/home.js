d939ae48e19ac8360ddfe824215e0d5a583a61ea
Esteban Pacheco <A20216403@alumnos.uady.mx>
2023-08-02 22:33:24 -0600

    save new dates in localstorage

--- Tareas/home.js
+++ Tareas/home.js
@@ -83,6 +83,8 @@ function renderEvents(){
             const id = button.getAttribute('data-id');
             events = events.filter(event => event.id !== id);
             
+            save(JSON.stringify(events));
+
             renderEvents();
         });
     });
